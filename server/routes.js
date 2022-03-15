d9730684f25f654b1fdf16d855f550291d58b770
Daniel Moura <daniel.lucio.moura@gmail.com>
2022-03-15 09:05:31 -0300

    wip Aula 01

---
+++ server/routes.js
@@ -0,0 +1,65 @@
+import config from './config.js'
+import { Controller } from './controller.js'
+import { logger } from './util.js'
+
+const {
+    location,
+    pages
+} = config
+
+const controller = new Controller()
+
+async function routes(request, response) {
+    const { method, url } = request
+
+    if (method === 'GET' && url === '/') {
+        response.writeHead(302, {
+            'Location': location.home
+        })
+
+        return response.end()
+    }
+
+    if (method === 'GET' && url === '/home') {
+        const { stream } = await controller.getFileStream(pages.homeHTML)
+    
+        // The default Content-Type of response is text/html 
+        return stream.pipe(response)
+    }
+
+    if (method === 'GET' && url === '/controller') {
+        const { stream } = await controller.getFileStream(pages.controllerHTML)
+    
+        // The default Content-Type of response is text/html 
+        return stream.pipe(response)
+    }
+
+    // files
+    if (method === 'GET') {
+        const {
+            stream, type
+        } = await controller.getFileStream(url)
+
+        return stream.pipe(response)
+    }
+
+    response.writeHead(404)
+    return response.end()
+}
+
+function handleError(error, response) {
+    if (error.message.includes('ENOENT')) {
+        logger.warn(`asset not found: ${error.stack}`)
+        response.writeHead(404)
+        return response.end()
+    }
+
+    logger.error(`unhandled error: ${error.stack}`)
+    response.writeHead(500)
+    return response.end()
+}
+
+export function handler(request, response) {
+    return routes(request, response)
+        .catch(error => handleError(error, response))
+}
